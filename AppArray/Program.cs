3d9e0c61bbf8272e8c420eab37d3bd853928b65a
Nina <ninkr@list.ru>
2020-09-26 21:24:03 +0300

    New solution

---
+++ AppArray/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Globalization;
+
+namespace AppArray
+{
+    class Program
+    {
+        static void Main()
+        {
+            int N, K;
+            string str;
+            List<int> arr = new List<int>();
+            try
+            {
+                using (StreamReader input = new StreamReader("input.txt"))
+                {
+                    N = Convert.ToInt32(input.ReadLine());
+
+                    str = input.ReadLine();
+
+                    for (int i = 0; i < N; i++)
+                    {
+                        arr.Add(Convert.ToInt32(str.Split(' ')[i]));
+                    }
+
+                    K = Convert.ToInt32(input.ReadLine());
+                }
+                Console.WriteLine("Данные из файла получены.");
+            }
+            catch
+            {
+                Console.WriteLine("Ошибка! Проверьте наличие файла input.txt");
+                return;
+            }
+
+            for (int i = 1; i < N; i++)
+            {
+                int tmp1 = arr[i];
+
+                for (int j = i; j > 0 && arr[j-1] < arr[j]; j--)
+                {
+                    int tmp2 = arr[j-1];
+                    arr[j-1] = arr[j];
+                    arr[j] = tmp2;
+               
+                }
+            }
+
+            using (StreamWriter output = new StreamWriter("output.txt"))
+            {
+                for (int i = 0; i < K; i++)
+                {
+                    output.Write(arr[i] + " ");
+                }
+            }
+
+            Console.WriteLine("Файл output.txt сформирован.");
+
+            Console.ReadKey();
+        }
+    }
+}
