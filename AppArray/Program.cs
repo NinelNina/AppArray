372b1529ffe38a21e6593ef32549b45f902014df
Nina <ninkr@list.ru>
2020-10-05 14:05:57 +0300

    Add sort with using SortedSet

--- AppArray/Program.cs
+++ AppArray/Program.cs
@@ -1,10 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.IO;
-using System.Globalization;
+using System.Diagnostics.CodeAnalysis;
+
 
 namespace AppArray
 {
@@ -14,6 +12,7 @@ namespace AppArray
         {
             int N, K;
             string str;
+                                  
             List<int> arr = new List<int>();
             try
             {
@@ -41,30 +40,57 @@ namespace AppArray
                 return;
             }
 
-            for (int i = 1; i < N; i++)
-            {
-                int tmp1 = arr[i];
+            SortedSet<ValueIndexPair> sorted = new SortedSet<ValueIndexPair>(new ValueIndexComparer());
 
-                for (int j = i; j > 0 && arr[j-1] < arr[j]; j--)
-                {
-                    int tmp2 = arr[j-1];
-                    arr[j-1] = arr[j];
-                    arr[j] = tmp2;
-               
-                }
+            for (int i = 0; i < N; i++)
+            {
+               var pair = new ValueIndexPair(arr[i], i);
+               if (sorted.Count < K)
+               {
+                    sorted.Add(pair);
+               }
+               else
+               {
+                    if(sorted.Max.Value < arr[i])
+                    {
+                        sorted.Remove(sorted.Max);
+                        sorted.Add(pair);
+                    }
+               }
             }
 
             using (StreamWriter output = new StreamWriter("output.txt"))
             {
-                for (int i = 0; i < K; i++)
+                foreach (var i in sorted)
                 {
-                    output.Write(arr[i] + " ");
+                    output.Write(i.Value + " ");
                 }
             }
 
             Console.WriteLine("Файл output.txt сформирован.");
 
             Console.ReadKey();
+        }
+
+        class ValueIndexComparer : IComparer<ValueIndexPair>
+        {
+            public int Compare(ValueIndexPair x, ValueIndexPair y)
+            {
+                if (x.Value != y.Value) return -x.Value.CompareTo(y.Value);
+                return x.Index.CompareTo(y.Index);
+            }
+        }
+
+        struct ValueIndexPair
+        {
+            public readonly int Value;
+            public readonly int Index;
+
+            public ValueIndexPair(int value, int index)
+            {
+                Value = value;
+                Index = index;
+            }
         }
     }
 }
